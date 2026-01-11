aa47997712b341a78e51b64e9b39a95fa9df555b
atili <atili@AT_DESKTOP>
2026-01-11 11:45:22 -0300

    Adicionar arquivos de projeto.

---
+++ Data/TodoService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace HybridTodoApp.Data;
+
+public class TodoService {
+    string file = string.Empty;
+
+    public TodoService() {
+        file = Path.Combine(Environment.GetFolderPath
+            (Environment.SpecialFolder.ApplicationData), "items.json");
+    }
+
+    public void SaveItems(List<TodoItem> items) {
+        File.WriteAllText(file, JsonSerializer.Serialize(items));
+    }
+
+    public IEnumerable<TodoItem> LoadItems() {
+        if (!File.Exists(file)) {
+            return Enumerable.Empty<TodoItem>();
+        }
+
+        var itemJson = File.ReadAllText(file);
+        return JsonSerializer.Deserialize<IEnumerable<TodoItem>>(itemJson) 
+            ?? Enumerable.Empty<TodoItem>();
+    }
+}
+
